6d47be2097341973b9c6c3a829342d16e7a88771
lock009 <mannrajveersingh69@gmail.com>
2025-11-27 23:44:56 +0530

    new

--- Sources/GroceryAppSharedModels/GroceryItemRequest.swift
+++ Sources/GroceryAppSharedModels/GroceryItemRequest.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+public struct GroceryItemRequest : Codable {
+    public var title : String
+    public var price : Int
+    public var quantity : Double
+    
+    public init(title: String, price: Int, quantity: Double) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
--- Sources/GroceryAppSharedModels/GroceryItemResponse.swift
+++ Sources/GroceryAppSharedModels/GroceryItemResponse.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+public struct GroceryItemResponse : Codable {
+    public var id : UUID
+    public var title : String
+    public var price : Int
+    public var quantity : Double
+    
+   public init(id: UUID, title: String, price: Int, quantity: Double) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
