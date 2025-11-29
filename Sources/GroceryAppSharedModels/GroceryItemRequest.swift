7567ce0dd048c20d6ca12f3f699fa3a767dc4bf0
lock009 <mannrajveersingh69@gmail.com>
2025-11-29 18:11:19 +0530

    value difference

--- Sources/GroceryAppSharedModels/GroceryItemRequest.swift
+++ Sources/GroceryAppSharedModels/GroceryItemRequest.swift
@@ -9,10 +9,10 @@ import Foundation
 
 public struct GroceryItemRequest : Codable {
     public var title : String
-    public var price : Int
-    public var quantity : Double
+    public var price : Double
+    public var quantity : Int
     
-    public init(title: String, price: Int, quantity: Double) {
+    public init(title: String, price: Double, quantity: Int) {
         self.title = title
         self.price = price
         self.quantity = quantity
--- Sources/GroceryAppSharedModels/GroceryItemResponse.swift
+++ Sources/GroceryAppSharedModels/GroceryItemResponse.swift
@@ -10,10 +10,10 @@ import Foundation
 public struct GroceryItemResponse : Codable {
     public var id : UUID
     public var title : String
-    public var price : Int
-    public var quantity : Double
+    public var price : Double
+    public var quantity : Int
     
-   public init(id: UUID, title: String, price: Int, quantity: Double) {
+   public init(id: UUID, title: String, price: Double, quantity: Int) {
         self.id = id
         self.title = title
         self.price = price
